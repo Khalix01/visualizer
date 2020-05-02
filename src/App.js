53bb67675ec0653a22b1af1c845315a2598f4281
khalix <krutikshah01@gmail.com>
2020-05-02 10:56:25 -0400

    idk

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> i want to kms.
+          Wrok damn it<code>src/App.js</code> i want to kms.
         </p>
         <a
           className="App-link"
